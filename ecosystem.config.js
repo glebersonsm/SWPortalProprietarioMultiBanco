ff998a5f510fc7020bd0d09ce0c474dd4ab06e04
Glebersonsm <glebersonsm@gmail.com>
2025-11-02 12:44:24 -0300

    aa

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,21 +2,9 @@ module.exports = {
   apps: [
     {
       name: 'PortalClienteGrGroup',
-      // Para Windows, o ideal é rodar o script diretamente.
-      // O comando "npm run start" no Windows é um script .cmd.
-      // Por isso, rodamos o comando next start diretamente.
-      script: 'npm', // Ou 'node' se preferir
-      args: ['run', 'start'], // Argumentos para o 'npm'
-      cwd: './', // O diretório de trabalho deve ser a raiz do projeto
-      instances: 1, // Número de instâncias a rodar
-      exec_mode: 'fork', // Modo de execução
-      autorestart: true, // Garante que o PM2 reinicie em caso de falha
-      watch: false,
-      max_memory_restart: '1G',
-      env: {
-        NODE_ENV: 'production',
-        PORT: 3000, // Porta em que seu Next.js deve rodar (ajuste se necessário)
-      },
+      script: './node_modules/next/dist/bin/next', // Executa o binário do next
+      args: ['start', '-p', '3000'], // Passa os argumentos para ele
+      // ... outras configurações
     },
   ],
 };
